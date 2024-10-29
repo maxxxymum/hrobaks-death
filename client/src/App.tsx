4867740462d10a3be3a765e0a99baa40db648a1a
Max <maksymm@wix.com>
2024-10-29 21:13:32 +0200

    feat: login screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Theme, Heading, Text, Flex, Separator, Box, TextField, Button } from "@radix-ui/themes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Theme appearance="dark" accentColor="tomato">
+      <Flex direction={'column'} align={'stretch'}>
+        <Flex direction='column' align={'center'}>
+          <Text>welcome to</Text>
+          <Heading color="red">HROBAKS DEATH</Heading>
+        </Flex>
+        <Box py={'2'}>
+          <Separator size={'4'} />
+        </Box>
+
+        <Flex direction={'column'} flexGrow={'1'} justify={'center'} align={'center'}>
+
+          <Heading as="h2" size={'4'}>Login</Heading>
+          <Text>Plane name</Text>
+          <TextField.Root placeholder="Plane name" />
+          <Button>Let's go</Button>
+        </Flex>
+      </Flex>
+    </Theme>
   )
 }
 
