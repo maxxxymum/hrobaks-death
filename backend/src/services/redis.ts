bbd0b0ec0c5ca592b676d9621b234092c97b1159
Max <maksymm@wix.com>
2024-10-30 22:22:58 +0200

    fix: redis setup

--- backend/src/services/redis.ts
+++ backend/src/services/redis.ts
@@ -1,7 +1,9 @@
 import { createClient } from 'redis';
 
 function createRedisClient() {
-    const redisClient = createClient({ url: 'redis://127.0.0.1:6379' });
+    const redisClient = createClient(({
+        url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    }));
 
     redisClient.on('error', (err) => console.error('Redis Client Error', err));
     redisClient.connect().then(() => console.log('Connected to Redis'));
